0c7f5e29dd6f36c23fd49d3a846aa96829ec3630
Shantanu Kumar <kumar.shantanu@gmail.com>
2020-10-13 00:02:14 +0530

    use bract.core 0.6.2-alpha3 and corresponding config

--- resources/leiningen/new/bract/project.clj
+++ resources/leiningen/new/bract/project.clj
@@ -6,15 +6,16 @@
   :resource-paths ["resources" "target/generated/resources"]  ; see :project-edn entry
   :pedantic?    :warn
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [bract/bract.core "0.6.2-alpha2"]]
+                 [bract/bract.core "0.6.2-alpha3"]]
   :target-path "target/%s"
   :plugins [[lein-project-edn "0.2.0"]]
   :hooks [leiningen.project-edn/activate]
   :project-edn {:output-file "target/generated/resources/project.edn"
                 :verify-edn? true}
-  :main ^:skip-aot bract.core.main
-  :profiles {:dev     {:dependencies [[bract/bract.dev "0.6.2-0.2.0-alpha2"]]
+  :profiles {:dev     {:dependencies [[bract/bract.dev "0.6.2-0.2.0-alpha3"]]
+                       :main ^:skip-aot bract.core.dev
                        :repl-options {:port 3001
-                                      :init-ns bract.core.dev}}
+                                      :init-ns bract.dev.repl}}
              :uberjar {:aot [bract.core.main]
+                       :main ^:skip-aot bract.core.main
                        :pedantic? :abort}})
